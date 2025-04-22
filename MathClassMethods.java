e9cd80b7cf77bb50dff54aa1a27ad5b8d399146e
Somesh Marathe <138389930+Someshmarathe@users.noreply.github.com>
2025-04-22 16:32:35 +0530

    Add files via upload

---
+++ MathClassMethods.java
@@ -0,0 +1,12 @@
+public class MathClassMethods {
+    public static void main(String[] args){
+        int a = 20 , b = 36;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.round(10.16));
+        System.out.println(Math.sqrt(b));
+        System.out.println((int)(Math.random()*10));
+        System.out.println(Math.pow(a,2));
+        System.out.println(Math.abs(-(a)));
+    }
+}
